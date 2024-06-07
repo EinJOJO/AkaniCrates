93a345e6245592eea2dd0fa441bc4c3c73fb7485
EinJOJO <schlotmax507@gmail.com>
2024-06-07 15:28:12 +0200

    opening effect

---
+++ src/main/java/it/einjojo/akani/crates/crate/effect/open/EpicOpeningEffect.java
@@ -0,0 +1,26 @@
+package it.einjojo.akani.crates.crate.effect.open;
+
+import mc.obliviate.inventory.Gui;
+
+public class EpicOpeningEffect implements OpeningEffect {
+
+    @Override
+    public Gui gui() {
+        return null;
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void onEnd() {
+
+    }
+}
